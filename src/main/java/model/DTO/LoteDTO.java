8c61339e592b9eee1d3d16e50d369ce56bfb0cae
i2hon <ian2honcharuk@gmail.com>
2026-05-24 16:19:49 -0300

    cree el actualizar en EmpresaDAOJDBC y cree los DTO de empresa, lote, representste de empresa, rol y usuario

--- src/main/java/model/DTO/LoteDTO.java
+++ src/main/java/model/DTO/LoteDTO.java
@@ -1,10 +1,50 @@
 package model.DTO;
 
-public record LoteDTO(
-        Integer id,
-        String ubicacion,
-        Double superficie,
-        String estado,
-        String infraestructura
-) {
+public record LoteDTO (
+    int id,
+    double latitud,
+    double longitud,
+    double altitud,
+    double superficie,
+    String estado,
+    String infraestructura) {
+
+    public LoteDTO(int id, double latitud, double longitud, double altitud,
+                   double superficie, String estado, String infraestructura) {
+        this.id = id;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.altitud = altitud;
+        this.superficie = superficie;
+        this.estado = estado;
+        this.infraestructura = infraestructura;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public double getAltitud() {
+        return altitud;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getInfraestructura() {
+        return infraestructura;
+    }
 }
